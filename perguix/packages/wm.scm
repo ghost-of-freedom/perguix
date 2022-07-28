6ffca7083594aeb1e4c76cf89d35bff80a1ef7ab
ghost-of-freedom <97169326+ghost-of-freedom@users.noreply.github.com>
2022-07-28 18:53:54 +0200

    Add i3-easyfocus

--- perguix/packages/wm.scm
+++ perguix/packages/wm.scm
@@ -42,3 +42,24 @@
    (synopsis "A C interface library to i3wm")
    (description "i3ipc-GLib is a C library for controlling the window manager. This project is intended to be useful in applications such as status line generators, pagers, notification daemons, scripting wrappers, external controllers, dock windows, compositors, config templaters, and for debugging or testing the window manager itself.")
    (license gpl3)))
+
+(define-public perguix-i3-easyfocus
+  (package
+   (name "perguix-i3-easyfocus")
+   (version "0")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/ghost-of-freedom/i3-easyfocus")
+           (commit "77441e8e92b8c8e0077dda2d35a335115835676d")))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "1s5n8sxrp23h6dcq4bwvv35y06dkzpl10v5vy1kj2x7bfr0cl1rj"))))
+   (build-system gnu-build-system)
+   (inputs (list perguix-i3ipc-glib xcb-util-keysyms libxcb))
+   (native-inputs (list perguix-i3ipc-glib xcb-util-keysyms libxcb))
+   (home-page "https://github.com/cornerman/i3-easyfocus")
+   (synopsis "Focus and select windows in i3 ")
+   (description "Draws a small label ('a'-'z') on top of each visible container, which can be selected by pressing the corresponding key on the keyboard (cancel with ESC). By default, only windows on the current workspace are labelled.")
+   (license gpl3)))
